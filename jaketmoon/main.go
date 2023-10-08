9a67ccad127095a40c0b5a884afb83a7da181072
XYF <xieyifeng@aliyun.com>
2023-10-08 18:50:59 +0800

    feat:finish task

--- jaketmoon/main.go
+++ jaketmoon/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"strconv"
 )
 
+// ///////////////////////////////////////////////////////////////////////////////////
 type TODO struct {
 	Content    string `json:"content"`
 	Done       bool   `json:"done"`
@@ -20,11 +23,9 @@ type UserInformation struct {
 	IdentityNumber string `json:"identity_number"`
 }
 
-func Register(c *gin.Context) {
-	var userinformation UserInformation
-	c.BindJSON(&userinformation)
-	users = append(users, userinformation)
-	c.JSON(200, gin.H{"状态": "ok", "已成功添加用户为": userinformation})
+// ////////////////////////////////////////////////////////////////////////////////////////
+func register() {
+
 }
 
 func Tianjia(c *gin.Context) {
@@ -58,15 +59,47 @@ func Chaxun(c *gin.Context) {
 	c.JSON(200, gin.H{"状态": "ok", "查询值为": todos[index]})
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+
 // 切片
 var todos []TODO
 var users []UserInformation
 
+// /////////////////////////////////////////////////////////////////////////
 func main() {
 	r := gin.Default() //创建一个默认路由
-	//绑定路由规则和函数，访问index的路由，将有对应的函数去处理
+
+	//连接mysql数据库
+	var conn *gorm.DB
+	conn, err := gorm.Open("mysql", "root:xyf1029@tcp(127.0.0.1:3306)/userinformation")
+	if err != nil { //连接失败
+		fmt.Println("gorm.Open err", err)
+		return
+	}
+
+	conn.DB().SetMaxIdleConns(20)  //初始连接数
+	conn.DB().SetMaxIdleConns(200) //最大连接数
+
+	defer conn.Close() //关闭数据库
+	conn.SingularTable(true)
+	fmt.Println(conn.AutoMigrate(new(UserInformation)).Error) //使用AutoMigrate ()方法来实现数据库表迁移，可以自动增加表中没有的字段和索引，在Gin main.go函数中使用非常方便，不用手动运行迁移了
+
 	//用户注册
-	r.POST("/users", Register)
+	r.POST("/users", func(c *gin.Context) {
+		var userinformation UserInformation
+		c.BindJSON(&userinformation)
+		users = append(users, userinformation)
+		c.JSON(200, gin.H{"状态": "ok", "已成功添加用户为": userinformation})
+
+		//接下来是数据库的操作
+		res := conn.Create(&userinformation) //保存数据到user_information
+		fmt.Println(res.Error)               //查看是否有错误
+		fmt.Println(res.RowsAffected)        //查看影响的行数
+
+	})
+
+	//绑定路由规则和函数，访问index的路由，将有对应的函数去处理
+
 	//新增TODO
 	r.POST("/todo", Tianjia)
 
@@ -84,3 +117,5 @@ func main() {
 
 	r.Run(":8080") //运行
 }
+
+////////////////////////////////////////////////////////////////////////
