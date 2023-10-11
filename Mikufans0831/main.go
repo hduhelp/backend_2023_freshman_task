1746e2bad6e7de0b9adce2a686c4733e63414997
Mikufans0831 <1063243756@qq.com>
2023-10-11 18:56:30 +0800

    message

--- Mikufans0831/main.go
+++ Mikufans0831/main.go
@@ -1,136 +1,233 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
+	"gopkg.in/gomail.v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var timeTemplate1 string = "2006-01-02 15:04:05"
+
 type TODO struct {
+	ID       int
 	Content  string `json:"content"`
-	Done     bool   `json:"done"`
+	Done     bool   `json:"done"` //`"DEFAULT:true"`
 	Deadline string `json:"deadline"`
+	Priority int    `json:"priority"`
+	Unix     int64
+	Late     bool `json:"Late"`
 }
 
-var todos []TODO
-var blink []int
-var bl TODO
-var ptodos []TODO
-
-// 计算时间
-func time(a string) int {
-	year, _ := strconv.Atoi(a[0:4])
-	mouth, _ := strconv.Atoi(a[5:7])
-	day, _ := strconv.Atoi(a[8:10])
-	hour, _ := strconv.Atoi(a[11:13])
-	minute, _ := strconv.Atoi(a[14:16])
-	second, _ := strconv.Atoi(a[17:19])
-	sum := year*31536000 + mouth*2592000 + day*86400 + hour*3600 + minute*60 + second
-	return sum
+// 返回一个支持至 秒 级别的 cron
+func newWithSeconds() *cron.Cron {
+	secondParser := cron.NewParser(cron.Second | cron.Minute |
+		cron.Hour | cron.Dom | cron.Month | cron.DowOptional | cron.Descriptor)
+	return cron.New(cron.WithParser(secondParser), cron.WithChain())
 }
+
+// 每日委托
+func meiri(db *gorm.DB) {
+	var todos []TODO
+	db.Where("priority = ?", 3).Delete(&todos)
+	db.Delete(&todos)
+
+	tomUnix := time.Now().Unix() + 86400
+	o := TODO{
+		Content:  "原神委托。",
+		Done:     false,
+		Unix:     tomUnix,
+		Priority: 3,
+	}
+	o.Deadline = time.Unix(tomUnix, 0).Format("2006-01-02 15:04:05")
+
+	Bt := TODO{
+		Content:  "崩铁活跃度。",
+		Done:     false,
+		Unix:     tomUnix,
+		Priority: 3,
+	}
+	Bt.Deadline = time.Unix(tomUnix, 0).Format("2006-01-02 15:04:05")
+
+	BA := TODO{
+		Content:  "BA日常。",
+		Done:     false,
+		Unix:     tomUnix,
+		Priority: 3,
+	}
+	BA.Deadline = time.Unix(tomUnix, 0).Format("2006-01-02 15:04:05")
+	db.Create(&o)
+	db.Create(&Bt)
+	db.Create(&BA)
+}
+
+// 更新每日委托
+func up(db *gorm.DB) {
+	c := newWithSeconds()
+	spec := "0 0 4 * * *"
+	// spec := "5 */2 * * * ?"
+	c.AddFunc(spec, func() {
+		meiri(db)
+	})
+	c.Start()
+}
+
+// 逾期
+func late(db *gorm.DB) {
+	var todos []TODO
+	c := newWithSeconds()
+	spec := "0 0 * * * *"
+	// spec := "5 */2 * * * ?"
+	nowUnix := time.Now().Unix()
+	db.Where("unix  <= ? AND late <> ? AND done == ?", nowUnix, true, false).Find(&todos)
+	c.AddFunc(spec, func() {
+		db.Model(TODO{}).Where("unix <= ?", nowUnix).Updates(TODO{Late: true})
+	})
+	c.Start()
+}
+
+// 问候
+func send1(db *gorm.DB) {
+	var late []TODO
+	var no []TODO
+	var late1 string
+	var no1 string
+	db.Where("late = ?", true).Find(&late)
+	db.Where("done = ?", false).Find(&no)
+	for _, l := range late {
+		late1 += l.Content
+	}
+	for _, n := range no {
+		no1 += n.Content
+	}
+	if late1 != "" && no1 != "" {
+		message := `
+		    <p> Hello %s,</p>
+
+			<p style="text-indent:2em">未完成事项：</p>
+			<p style="text-indent:2em">` + no1 + `</p>
+
+			<p style="text-indent:2em">逾期事项：</P>
+			<p style="text-indent:2em">` + late1 + `</p>
+			`
+		host := "smtp.qq.com"
+		port := 25
+		userName := "1063243756@qq.com"
+		password := "pdwbaqkhqshlbejg"
+
+		m := gomail.NewMessage()
+		m.SetHeader("From", userName) // 发件人
+		m.SetHeader("To", "1063243756@qq.com")
+		m.SetHeader("Subject", "TODOList") // 邮件主题
+		m.SetBody("text/html", fmt.Sprintf(message, "testUser"))
+
+		// text/plain的意思是将文件设置为纯文本的形式，浏览器在获取到这种文件时并不会对其进行处理
+
+		d := gomail.NewDialer(
+			host,
+			port,
+			userName,
+			password,
+		)
+		// 关闭SSL协议认证
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+		if err := d.DialAndSend(m); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// 定时问候
+func send2(db *gorm.DB) {
+	c := newWithSeconds()
+	spec := "0 0 * * * *"
+	// spec := "5 */2 * * * ?"
+	c.AddFunc(spec, func() {
+		send1(db)
+	})
+	c.Start()
+}
+
 func main() {
 	r := gin.Default()
-	bl.Done = true
+	dsn := "root:Qq563696767@tcp(127.0.0.1:3306)/go_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&TODO{})
+
+	go up(db)
+	go late(db)
+	go send2(db)
 
 	// 添加 TODO
 	r.POST("/todo", func(c *gin.Context) {
 		var todo TODO
 		c.BindJSON(&todo)
-		if len(blink) > 0 {
-			todos[blink[0]] = todo
-			blink = blink[1:]
-		} else {
-			todos = append(todos, todo)
-		}
+		Un, _ := time.ParseInLocation(timeTemplate1, todo.Deadline, time.Local)
+		todo.Unix = Un.Unix()
+		db.Create(&todo)
 		c.JSON(200, gin.H{"status": "ok"})
 	})
 
 	// 删除 TODO
 	r.DELETE("/todo/:index", func(c *gin.Context) {
 		index, _ := strconv.Atoi(c.Param("index"))
-		if index <= len(todos)-1 && todos[index] != bl {
-			todos[index] = bl
-			blink = append(blink, index)
-		}
+		var todo TODO
+		db.Where("ID = ?", index).Delete(&todo)
 		c.JSON(200, gin.H{"status": "ok"})
 	})
 
 	// 修改 TODO
 	r.PUT("/todo/:index", func(c *gin.Context) {
 		var todo TODO
-		c.BindJSON(&todo)
+		var new TODO
+		c.BindJSON(&new)
 		index, _ := strconv.Atoi(c.Param("index"))
-		if index+1 > len(todos) {
-			n := len(todos)
-			for i := 0; i < index-n; i++ {
-				todos = append(todos, bl)
-				blink = append(blink, n+i)
-			}
-			todos = append(todos, todo)
-		} else {
-			if todos[index].Deadline == "" {
-				for j, v := range blink {
-					if v == index {
-						blink = append(blink[:j], blink[j+1:]...)
-					}
-				}
-			}
-		}
-		todos[index] = todo
+		db.Where("ID = ?", index).First(&todo)
+		Un, _ := time.ParseInLocation(timeTemplate1, todo.Deadline, time.Local)
+		todo.Unix = Un.Unix()
+		db.Model(&todo).Updates(new)
+
+		// db.Where(xiShu{Name: "LiuBei"}).Assign(xiShu{Age: 35}).FirstOrInit(&todo)
+
 		c.JSON(200, gin.H{"status": "ok"})
+
 	})
 
 	// 获取 TODO
 	r.GET("/todo", func(c *gin.Context) {
-		ptodos = nil
-		for i, _ := range todos {
-			if todos[i].Deadline != "" {
-				ptodos = append(ptodos, todos[i])
-			}
-		}
-
-		c.JSON(200, ptodos)
+		var todos []TODO
+		db.Find(&todos)
+		c.JSON(200, todos)
 	})
 
 	// 查询TODO
 	r.GET("/todo/:index", func(c *gin.Context) {
+		var todo TODO
 		index, _ := strconv.Atoi(c.Param("index"))
-		c.JSON(200, todos[index])
+		db.Where("id = ?", index).First(&todo)
+		c.JSON(200, todo)
+
 	})
 
 	//删除已完成TODO
 	r.DELETE("/todo", func(c *gin.Context) {
-		ptodos = nil
-		blink = nil
-		for i, _ := range todos {
-			if todos[i].Done == false {
-				ptodos = append(ptodos, todos[i])
-			}
-		}
-		todos = ptodos
+		var todos []TODO
+		db.Where("done = ?", true).Delete(&todos)
+		db.Delete(&todos)
 		c.JSON(200, gin.H{"status": "ok"})
 	})
-	//按照截止时间升序排序TODO
+	//按照优先级升序再按截止时间升序排序TODO
 	r.PUT("/todo", func(c *gin.Context) {
-		ptodos = nil
-		blink = nil
-		for i, _ := range todos {
-			if todos[i].Deadline != "" {
-				ptodos = append(ptodos, todos[i])
-			}
-		}
-		//冒泡排序
-		for i := 0; i < len(ptodos)-1; i++ {
-			for j := 0; j < len(ptodos)-i-1; j++ {
-				if time(ptodos[j].Deadline) > time(ptodos[j+1].Deadline) {
-					temp := ptodos[j]
-					ptodos[j] = ptodos[j+1]
-					ptodos[j+1] = temp
-				}
-			}
-		}
-		todos = ptodos
-		c.JSON(200, gin.H{"status": "ok"})
+		var todos []TODO
+		db.Order("priority").Order("unix").Find(&todos)
+		c.JSON(200, todos)
 	})
 
 	r.Run(":8080")
