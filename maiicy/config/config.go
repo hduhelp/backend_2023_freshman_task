2bde65b7c3d71338635d4589ec75009294f18daf
maiicy <Maiicy@Foxmail.com>
2023-10-14 11:53:58 +0800

    :sparkles: Support cli arguments

--- maiicy/config/config.go
+++ maiicy/config/config.go
@@ -1,8 +1,13 @@
 package config
 
-import "github.com/pelletier/go-toml"
+import (
+	"fmt"
+	"github.com/pelletier/go-toml"
+	"os"
+)
 
 type Config struct {
+	DBPath     string `toml:"db_path"`
 	SecretKey  string `toml:"secret_key"`
 	ServerIP   string `toml:"server_ip"`
 	ServerPort int    `toml:"server_port"`
@@ -18,6 +23,16 @@ func LoadConfig(filename string) (Config, error) {
 
 	if err := tree.Unmarshal(&config); err != nil {
 		return config, err
+	}
+
+	// 从环境变量中获取 SecretKey
+	secretKeyEnv := os.Getenv("SECRET_KEY")
+	if secretKeyEnv == "" {
+		fmt.Println("错误：未设置服务器的 JWT 秘钥 (SECRET_KEY)。")
+		fmt.Println("请设置环境变量 SECRET_KEY 以确保服务器的安全性。")
+		os.Exit(1) // 退出应用程序，因为缺少必要的秘钥
+	} else {
+		config.SecretKey = secretKeyEnv
 	}
 
 	return config, nil
--- maiicy/main.go
+++ maiicy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"login-system/config"
@@ -12,21 +13,39 @@ import (
 )
 
 func main() {
-
-	databasePath := "data.db"
+	// 读取配置文件
 	configFilename := "config.toml"
-
-	err := db_handle.ConnectDatabase(databasePath)
-	if err != nil {
-		panic(err)
-	}
-
 	MyConfig, err := config.LoadConfig(configFilename)
 	if err != nil {
 		fmt.Printf("无法读取配置文件：%v\n", err)
 		os.Exit(1)
 	}
 	utils.SecretKey = []byte(MyConfig.SecretKey)
+
+	databasePath := MyConfig.DBPath
+
+	// 读取cli参数
+	dbPath := flag.String("dbPath", "null", "数据库路径")
+	host := flag.String("host", "null", "服务器主机名")
+	port := flag.Int("port", -1, "服务器端口号")
+
+	flag.Parse()
+
+	// 如果cli中有输入，则用cli参数，如果没有输入，则用配置文件内数据
+	if *dbPath != "null" {
+		databasePath = *dbPath
+	}
+	if *host != "null" {
+		MyConfig.ServerIP = *host
+	}
+	if *port != -1 {
+		MyConfig.ServerPort = *port
+	}
+
+	err = db_handle.ConnectDatabase(databasePath)
+	if err != nil {
+		panic(err)
+	}
 
 	r := gin.Default()
 	routes.SetupRoutes(r)
